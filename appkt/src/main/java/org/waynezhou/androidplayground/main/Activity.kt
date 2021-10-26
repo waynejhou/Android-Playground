4649a380699348cb905c6ed1ed461bb8464519c6
Wayne_Zhou <Wayne_Zhou@compal.com>
2021-10-26 16:15:22 +0800

    1026

--- appkt/src/main/java/org/waynezhou/androidplayground/main/Activity.kt
+++ appkt/src/main/java/org/waynezhou/androidplayground/main/Activity.kt
@@ -1,23 +1,30 @@
 package org.waynezhou.androidplayground.main
 
+import android.content.ContentResolver
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import org.waynezhou.androidplayground.databinding.ActivityMainBinding
+import org.waynezhou.libutilkt.LogHelper
 import org.waynezhou.libviewkt.AppCompatActivityWrapper
 
 class Activity : AppCompatActivityWrapper() {
 
-    private val layout = Layout()
+    internal val layout = Layout()
+    internal val helloWorldButton = HelloWorldButton()
+    internal val mediaList = MediaList()
     override fun onInitComponents(savedInstanceState: Bundle?) {
         layout.init(this)
+        helloWorldButton.init(this)
+        mediaList.init(this)
     }
 
 }
 
 class Layout{
     private lateinit var host: Activity
-    private lateinit var binding: ActivityMainBinding
+    internal lateinit var binding: ActivityMainBinding
     internal fun init(activity:Activity){
         host = activity
         host.getEventGroup()
@@ -30,16 +37,45 @@ class Layout{
     }
 }
 
+class HelloWorldButton{
+    private lateinit var host: Activity
+    private lateinit var layout: Layout
+    private val binding: ActivityMainBinding
+        get()=layout.binding
+    internal fun init(activity:Activity){
+        host = activity
+        host.getEventGroup()
+            .on({it.create}, this::onHostCreate)
+        layout = host.layout
+    }
+    private fun onHostCreate(savedInstanceState: Bundle?){
+        LogHelper.i()
+        binding.mainHelloBtn.setOnClickListener{
+            LogHelper.d("Hello World")
+        }
+    }
+}
+
 class MediaList{
     private lateinit var host: Activity
     internal fun init(activity: Activity){
+        host = activity
         host.getEventGroup().on({it.create}, this::onHostCreate)
     }
 
     private fun onHostCreate(savedInstanceState: Bundle?) {
-        host.contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        val cancleSignal = CancellationSignal()
+        val cursor = host.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            null, Bundle.EMPTY, cancleSignal)
+        LogHelper.d("%b", cursor==null)
+        cursor?.run {
+            LogHelper.d(cursor.columnNames.joinToString(", "))
+            while(moveToNext().apply { LogHelper.d("Has Next: %b", this); }){
+                LogHelper.d(cursor.columnNames.joinToString(", "))
+            }
+            close()
+        }
 
-        )
     }
 }
